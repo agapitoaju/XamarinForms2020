82b22c3e5f06148fcf97d19e7282c77f05e46537
josea <josea@DESKTOP-AILB8UU>
2020-03-29 22:34:52 -0300

    App01 - Concluído

---
+++ Seção 05/App01_CEP/App01_CEP/App01_CEP/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_CEP.Servico.Modelo;
+using App01_CEP.Servico;
+
+namespace App01_CEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim();
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEndercoViaCEP(cep);
+
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {2},{3},{0},{1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO","O endereço não foi encontrado para o CEP: " + cep, "OK");
+                    }
+
+                    
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+                }
+
+            }
+
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+            if(cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP invádido! O CEP deve conter 8 caracteres.", "OK");
+                valido = false;               
+            }
+            else
+            {
+                valido =  true;
+            }
+
+            int novoCEP = 0;
+            if (!int.TryParse(cep, out novoCEP))
+            {
+                DisplayAlert("ERRO", "CEP invádido! O CEP deve ser composto apenas por números.", "OK");
+                valido = false;               
+            }
+            else
+            {
+                valido = true;
+            }
+           
+            return valido;
+        }
+
+
+    }
+}
